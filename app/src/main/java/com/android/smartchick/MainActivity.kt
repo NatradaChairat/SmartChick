04a5bf6ec1bb26409f54e958e42935469aaf4fee
Natrada Chairat <chairat.natrada@gmail.com>
2019-07-17 23:00:53 +0700

    Update ask permission

--- app/src/main/java/com/android/smartchick/MainActivity.kt
+++ app/src/main/java/com/android/smartchick/MainActivity.kt
@@ -1,30 +1,53 @@
 package com.android.smartchick
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.smartchick.authentication.AuthenticationActivity
 import android.content.Intent
 import android.os.Handler
+import android.widget.Toast
 import androidx.core.os.HandlerCompat.postDelayed
 import com.android.smartchick.authentication.AuthenticationFragment
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.PermissionNo
+import com.yanzhenjie.permission.PermissionYes
 
 
 class MainActivity : AppCompatActivity() {
+
+    protected val RQ_PERMISSION_BASE: Int = 4
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        AndPermission.with(this@MainActivity)
+                .requestCode(RQ_PERMISSION_BASE)
+                .permission(Manifest.permission.CAMERA)
+                .callback(this@MainActivity)
+                .start()
+
+    }
+
+    @PermissionYes(4)
+    private fun getPermissionYes(grantedPermissions: List<String>) {
         supportFragmentManager.findFragmentById(R.id.contentFrame) as AuthenticationFragment?
                 ?: AuthenticationFragment.newInstance().also {
                     supportFragmentManager.beginTransaction().apply {
                         replace(R.id.contentFrame, it).commit()
                     }
+
                 }
-
-
     }
+
+    @PermissionNo(4)
+    private fun getPermissionNo(deniedPermissions: List<String>) {
+        Toast.makeText(applicationContext, "No Permission ", Toast.LENGTH_SHORT).show()
+    }
+
+
 
 
 }
